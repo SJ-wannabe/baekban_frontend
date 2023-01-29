f0a1d2992eace128f51a9992c006747c5bffddee
cloud0406 <cloud0406@naver.com>
2023-01-29 16:33:40 +0900

    [Feat] 회원가입 화면 UI 개발(#9)
    * 회원가입 페이지 UI 개발
    * html / css 구현

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import App from './App';
-import Login from './pages/Login/Login';
+// import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <Login />
+    <Register />
   </React.StrictMode>,
 );
