57e3531e7b95d4426c9c1c003ffa067b0b897662
cloud0406 <cloud0406@naver.com>
2023-02-11 17:18:11 +0900

    [Feat, Style] 로그인 기능 개발(#14)
    * 로그인 기능
    * 회원가입, 로그인 페이지 폼 css 수정

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './Login.css';
 import {LockOutlined, UserOutlined} from '@ant-design/icons';
 import {Button, Form, Input, Card} from 'antd';
+import axios from 'axios';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -16,19 +17,44 @@ const StyleForm = styled(Form)`
 const StyleCard = styled(Card)`
   display: flex;
   width: 500px;
-  height: 370px;
+  height: 390px;
   margin: 100px auto;
+  position: relative;
+  padding: 0px;
 
   box-shadow: 5px 5px 10px 1px rgba(0, 0, 0, 0.6);
 `;
 
 const StyleButton = styled(Button)`
   width: 120px;
+  margin: 0px;
+`;
+
+const LoginStyleFormItem = styled(Form.Item)`
+  margin-bottom: 24px;
 `;
 
 function Login() {
+  const [failMsg, setFailMsg] = useState(false);
   const onFinish = (values: any) => {
     console.log('Received values of form: ', values);
+
+    axios
+      .post('http://localhost:3000/login', values)
+      .then(res => {
+        console.log(res.data);
+
+        const {accessToken} = res.data;
+
+        // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+        axios.defaults.headers.common[
+          'Authorization'
+        ] = `Bearer ${accessToken}`;
+      })
+      .catch(error => {
+        console.log(error);
+        setFailMsg(true);
+      });
   };
 
   return (
@@ -45,17 +71,17 @@ function Login() {
             initialValues={{remember: true}}
             onFinish={onFinish}
           >
-            <Form.Item
+            <LoginStyleFormItem
               className="loginInput"
-              name="username"
+              name="userid"
               rules={[{required: true, message: '아이디를 입력해주세요!'}]}
             >
               <Input
                 prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Username"
+                placeholder="UserID"
               />
-            </Form.Item>
-            <Form.Item
+            </LoginStyleFormItem>
+            <LoginStyleFormItem
               className="loginInput"
               name="password"
               rules={[{required: true, message: '비밀번호를 입력해주세요!'}]}
@@ -65,8 +91,8 @@ function Login() {
                 type="password"
                 placeholder="Password"
               />
-            </Form.Item>
-            <Form.Item>
+            </LoginStyleFormItem>
+            <LoginStyleFormItem>
               <StyleButton
                 type="primary"
                 htmlType="submit"
@@ -74,8 +100,11 @@ function Login() {
               >
                 Log in
               </StyleButton>
-            </Form.Item>
-            <Form.Item>
+            </LoginStyleFormItem>
+            {failMsg && <span>아이디 혹은 비밀번호가 일치하지 않습니다.</span>}
+            <LoginStyleFormItem
+              style={{position: 'absolute', bottom: 0, left: 30}}
+            >
               <div className="loginForm-account">
                 <Link to="/register">회원 가입</Link>
 
@@ -83,7 +112,7 @@ function Login() {
                   <Link to="/findAccount">아이디 / 비밀번호 찾기</Link>
                 </div>
               </div>
-            </Form.Item>
+            </LoginStyleFormItem>
           </StyleForm>
         </div>
       </div>
